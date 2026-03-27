c1810f728f2a1546b56cb76e7a88e6e22c78ad88
mafia-0001 <sambhavs538@gmail.com>
2026-03-27 23:15:09 +0530

    Update recursive_binary_search.py

--- unit 1/recursive_binary_search.py
+++ unit 1/recursive_binary_search.py
@@ -1,13 +1,22 @@
 def binary_search(arr, key, low, high):
+    # checking if element is present or not
     if low > high:
         return -1
-    mid = (low + high) // 2
+    mid = int((low + high) / 2)   # finding middle
     if arr[mid] == key:
         return mid
-    elif key < arr[mid]:
-        return binary_search(arr, key, low, mid - 1)
+    if key < arr[mid]:
+        # go to left part
+        return binary_search(arr, key, low, mid-1)
     else:
-        return binary_search(arr, key, mid + 1, high)
-
+        # go to right part
+        return binary_search(arr, key, mid+1, high)
+# main
 arr = [1,2,3,4,5,6]
-print(binary_search(arr, 4, 0, len(arr)-1))
+print("Array is:", arr)
+key = 4   # element to search
+res = binary_search(arr, key, 0, len(arr)-1)
+if res == -1:
+    print("Not found")
+else:
+    print("Found at index", res)
