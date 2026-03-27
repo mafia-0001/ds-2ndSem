e19e9a2f390918acbdaaa40a357a6f52551530ca
mafia-0001 <sambhavs538@gmail.com>
2026-03-27 23:21:17 +0530

    Update dll.py

--- unit 2/dll.py
+++ unit 2/dll.py
@@ -3,24 +3,21 @@ class Node:
         self.data = data
         self.prev = None
         self.next = None
-
 class DLL:
     def __init__(self):
         self.head = None
-
     def insert(self, data):
         new = Node(data)
         if self.head:
             self.head.prev = new
             new.next = self.head
         self.head = new
-
     def traverse(self):
         temp = self.head
         while temp:
-            print(temp.data, end=" <-> ")
+            print(temp.data)
             temp = temp.next
-
+#main
 dll = DLL()
 dll.insert(10)
 dll.insert(20)
